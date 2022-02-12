af4c6c198b3ce7bf4e407bdce92bdece5c8d905f
Jim Lyst <4419323+jlyst@users.noreply.github.com>
2022-02-11 20:32:34 -0500

    Update to use Papaparse lib
    Change in Google API broke our use of tabletop.js....

--- src/chatbot.js
+++ src/chatbot.js
@@ -5,49 +5,67 @@ var bot;
 var chatbot = {
   db: [],
   replyDelay: 800,
-  loadDB: function(link) {
-    Tabletop.init({
-      key: link,
-      callback: data => {
-        this.db = data["Sheet1"].elements;
-        console.log("Your Database Rows Loaded: ", this.db.length);
-      }
-    });
-  },
-  getDB: function(link, sheetName) {
+  getDB: function (link) {
     return new Promise((resolve, reject) => {
-      Tabletop.init({
-        key: link,
-        callback: data => {
-          var sheetNames = Object.keys(data);
-          var thisdb = [];
-          if (sheetName)
-            thisdb = data[sheetName] ? data[sheetName].elements : [];
-          else
-            thisdb = data[sheetNames[0]] ? data[sheetNames[0]].elements : [];
-          console.log("Your Database Rows Loaded: ", thisdb.length);
-          resolve(thisdb);
+      var self = this;
+      Papa.parse(link, {
+        download: true,
+        header: true,
+        complete: function (results) {
+          resolve(results.data);
         }
-      });
+      })
     });
   },
-  dbFilter: function (db,col,val) {
-    var filtered = db.filter(function(row) {
-      var match = true;
-      if (!Array.isArray(val)) val = [val];
-      val.forEach(v=>{
-        if (row[col].toLowerCase().indexOf(v.toLowerCase()) == -1)
-        match = false;
-      })
-      return match; 
+  // loadDB: function(link) {
+  //   Tabletop.init({
+  //     key: link,
+  //     callback: data => {
+  //       this.db = data["Sheet1"].elements;
+  //       console.log("Your Database Rows Loaded: ", this.db.length);
+  //     }
+  //   });
+  // },
+  // getDB: function(link, sheetName) {
+  //   return new Promise((resolve, reject) => {
+  //     Tabletop.init({
+  //       key: link,
+  //       callback: data => {
+  //         var sheetNames = Object.keys(data);
+  //         var thisdb = [];
+  //         if (sheetName)
+  //           thisdb = data[sheetName] ? data[sheetName].elements : [];
+  //         else
+  //           thisdb = data[sheetNames[0]] ? data[sheetNames[0]].elements : [];
+  //         console.log("Your Database Rows Loaded: ", thisdb.length);
+  //         resolve(thisdb);
+  //       }
+  //     });
+  //   });
+  // },
+  dbFilter: function (db, col, val) {
+    var filtered = db.filter(function (row) {
+      return row[col].toLowerCase().indexOf(val.toLowerCase()) > -1;
     });
     return filtered;
   },
-  loadFiles: function(filenames) {
+  dbFilter2: function (db, col, val) {
+    var filtered = db.filter(function (row) {
+      var match = true;
+      if (!Array.isArray(val)) val = [val];
+      val.forEach(v => {
+        if (row[col].toLowerCase().indexOf(v.toLowerCase()) == -1)
+          match = false;
+      })
+      return match;
+    });
+    return filtered;
+  },
+  loadFiles: function (filenames) {
     bot = new RiveScript();
     bot.loadFile(filenames).then(on_load_success).catch(on_load_error);
   },
-  getReply: function(text) {
+  getReply: function (text) {
     bot.reply(null, text).then(
       reply => {
         reply = reply.replace(/\n/g, "<br>");
@@ -58,16 +76,16 @@ var chatbot = {
       }
     );
   },
-  postReply: function(reply, delay) {
+  postReply: function (reply, delay) {
     if (!delay) delay = this.replyDelay;
     var rand = Math.round(Math.random() * 10000);
-    setTimeout(function() {
+    setTimeout(function () {
       $("#dialogue").append(
         "<div class='bot-row' id='" +
-          rand +
-          "'><span class='bot'>" +
-          reply +
-          "</span></div>"
+        rand +
+        "'><span class='bot'>" +
+        reply +
+        "</span></div>"
       );
       if (typeof pop !== "undefined") pop.play();
       if (typeof onChatbotReply === "function") onChatbotReply();
@@ -80,20 +98,20 @@ var chatbot = {
       );
     }, delay);
   },
-  sendMessage: function() {
+  sendMessage: function () {
     var text = $("#message").val();
     if (text.length === 0) return false;
     $("#message").val("");
     $("#dialogue").append(
       "<div class='user-row'><span class='user'>" +
-        this.escapeHtml(text) +
-        "</span></div>"
+      this.escapeHtml(text) +
+      "</span></div>"
     );
     $("#dialogue").animate({ scrollTop: $("#dialogue")[0].scrollHeight }, 200);
     this.getReply(text);
     return false;
   },
-  escapeHtml: function(text) {
+  escapeHtml: function (text) {
     return text
       .replace(/&/g, "&amp;")
       .replace(/</g, "&lt;")
